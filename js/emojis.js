a8429130ac45bd2e8b96f3b23775fcc08eb5f962
Sebastian Henao <sebastianhenaoloaiza@gmail.com>
2022-06-06 14:27:54 -0500

    New idea :tada:

---
+++ js/emojis.js
@@ -0,0 +1,6 @@
+let typed = new Typed('#emoji',{
+    strings:["&#128190;","&#127797;","&#127857;"],
+    backSpeed: 200,
+    typeSpeed: 200,
+    loop: true
+});
--- js/type.js
+++
@@ -1,6 +0,0 @@
-let typed = new Typed('#typed',{
-    strings:["Sebastián.","Dev.","Student."],
-    backSpeed: 50,
-    typeSpeed: 50,
-    loop: true
-});
